e36578e04fc666dbefb8c8cc72f67e457366268a
BradHacker <harkerbd@gmail.com>
2018-06-14 19:51:25 -0400

    Update Syntax.

--- bot.js
+++ bot.js
@@ -13,54 +13,55 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.author.bot) return;
 
-  let guildID = message.guild.id;
-  retrieveNonChristianWords(guildID)
+  const m = message.content.toLowerCase().trim();
 
-  const args = message.content.trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+  if(m.indexOf(':') !== -1) {
+    let guildID = message.guild.id;
+    retrieveNonChristianWords(guildID)
 
-  if (message.member.permissions.has("ADMINISTRATOR")) {
-    //console.log("Member is owner: " + (message.member === message.guild.owner))
-    if(command === "bee_movie_script" && message.member === message.guild.owner){
-      message.channel.send("Give me a sec...");
-      let script = beeMovie.script.split('');
-      //console.log(script.length)
-      let scriptPieces = [];
-      let lettercount = 0;
-      let mes = '';
-      for(let i = 0; i < script.length; i++) {
-        if (lettercount % 10000 === 0 && lettercount !== 0) {
-          message.channel.send("Letter Count: " + lettercount)
-        }
-        if (lettercount < 2000 && i < script.length - 1) {
-          mes += script[i]
-          //console.log(script[i])
-          lettercount += 1;
-        } else {
-          if (i === script.length - 1) {
-            mes += script[i]
+    const args = m.slice(m.indexOf(':')+1, m.length).split(',');
+    const command = m.slice(0, m.indexOf(':'));
+    console.log('ARGS LENGTH: ' + args.length);
+    console.log('COMMAND: ' + command)
+
+    if (message.member.permissions.has("ADMINISTRATOR")) {
+      //console.log("Member is owner: " + (message.member === message.guild.owner))
+      if(command === "bee_movie_script" && message.member === message.guild.owner){
+        message.channel.send("Give me a sec...");
+        let script = beeMovie.script.split('');
+        //console.log(script.length)
+        let scriptPieces = [];
+        let lettercount = 0;
+        let mes = '';
+        for(let i = 0; i < script.length; i++) {
+          if (lettercount % 10000 === 0 && lettercount !== 0) {
+            message.channel.send("Letter Count: " + lettercount)
           }
-          scriptPieces.push(mes);
-          mes = script[i];
-          lettercount = 1;
-        }
-      }
-      for(i = 0; i < scriptPieces.length; i++) {
-        if (scriptPieces[i] !== '') {
-          message.channel.send(scriptPieces[i]);
-        }
-      }
-      return;
-    }
-    if(command === "remove") {
-      if(args[0] === 'the' && args[1] === 'ban' && args[2] === 'on') {
-        if(args[3]) {
-          let unbanned_words = [];
-          for(let i = 3; i < args.length; i += 2) {
-            unbanned_words.push(args[i]);
-            if(args[i+1] !== 'and') {
-              break;
+          if (lettercount < 2000 && i < script.length - 1) {
+            mes += script[i]
+            //console.log(script[i])
+            lettercount += 1;
+          } else {
+            if (i === script.length - 1) {
+              mes += script[i]
             }
+            scriptPieces.push(mes);
+            mes = script[i];
+            lettercount = 1;
+          }
+        }
+        for(i = 0; i < scriptPieces.length; i++) {
+          if (scriptPieces[i] !== '') {
+            message.channel.send(scriptPieces[i]);
+          }
+        }
+        return;
+      }
+      if(command === "unban") {
+        if(args.length > 0) {
+          let unbanned_words = [];
+          for(let i = 0; i < args.length; i++) {
+            unbanned_words.push(args[i].trim())
           }
           let real_unbaned_words = [];
           for(let i = 0; i < non_christian_words.length; i++) {
@@ -70,30 +71,27 @@ client.on('message', message => {
               i -= 1;
             }
           }
-          let words_to_write = non_christian_words.join('|');
-          fs.writeFile(`./${guildID}.txt`, words_to_write, function(err) {
-            if(err) {
-              message.channel.send('There was an error unbanning the words.')
-            }
-            retrieveNonChristianWords(guildID);
-            message.channel.send('Okay the following words have been unbanned: **' + real_unbaned_words.join(', ') + '**')
-
-          }); 
+          if(real_unbaned_words.length > 0) {
+            let words_to_write = non_christian_words.join('|');
+            fs.writeFile(`./${guildID}.txt`, words_to_write, function(err) {
+              if(err) {
+                message.channel.send('There was an error unbanning the words.')
+              }
+              retrieveNonChristianWords(guildID);
+              message.channel.send('Okay the following words have been unbanned: **' + real_unbaned_words.join(', ') + '**')
+            }); 
+          } else {
+            message.channel.send('Hmm... looks like none of those words are currently banned.')      
+          }
         } else {
           message.channel.send("Hmmm... It seems you didn't give me a word to unban")
         }
       }
-      return;
-    }
-    if(command === "ban") {
-      if(args[0] === 'the' && ( args[1] === 'word' || args[1] === 'words')) {
-        if(args[2]) {
+      if(command === "ban") {
+        if(args.length > 0) {
           let banned_words = [];
-          for(let i = 2; i < args.length; i += 2) {
-            banned_words.push(args[i]);
-            if(args[i+1] !== 'and') {
-              break;
-            }
+          for(let i = 0; i < args.length; i++) {
+            banned_words.push(args[i].trim())
           }
           for(let i = 0; i < banned_words.length; i++) {
             if(non_christian_words.indexOf(banned_words[i]) !== -1) {
@@ -113,25 +111,22 @@ client.on('message', message => {
           message.channel.send("Hmmm... It seems you didn't give me a word to ban")
         }
       }
-      return;
-    }
-    if(command === "list") {
-      if(args[0] === 'banned' && args[1] === 'words') {
+      if(command === "list all") {
         if(non_christian_words.length > 0) {
           message.channel.send('Okay the following word(s) are currently banned: **' + non_christian_words.join(', ') + '**')
         } else {
           message.channel.send("There currently aren't any banned words.")
         }
       }
-      return;
-    }
-    if(command === "set") {
-      if(args[0] === 'timeout' && args[1] === 'to') {
-        config.deleteTimeout = parseInt(args[2])
+      if(command === "set timeout") {
+        config.deleteTimeout = parseInt(args[0])
         console.log(JSON.stringify(config))
         fs.writeFile('./config.json', "" + JSON.stringify(config), () => {
           message.channel.send("Deletion timeout set to " + config.deleteTimeout + ' secs')
         })
+      }
+      if(command === "help") {
+        message.channel.send('**Barry B. Benson Bot Help**\n`ban: words` - bans words after the `:`, use commas to separate words/phrases\n`unban: words` - unbans words  after the `:` if they are currently in ban list, use commas to separate words/phrases\n`list all:` - lists all currently banned words\n`set timeout: number` - sets bot message tiemout in seconds\n`help:` - dsiplays this help screen')
       }
     }
   }
