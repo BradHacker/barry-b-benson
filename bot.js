d3f3b49bb869fdf6c56145cdc05a55e73f53b0a2
BradHacker <harkerbd@gmail.com>
2018-08-01 16:56:07 -0400

    Add Music Support

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core')
+const request = require('superagent')
 const client = new Discord.Client();
 const config = require('./config.json')
 const beeMovie = require('./beeMovieScript.json')
 const fs = require('fs');
+require('dotenv').config();
+
 let non_christian_words = [];
+let voiceChannel = null;
+let ytAudioQueue = [];
+let playing = false
 
 client.on('ready', () => {
   console.log('Beam me up Scotty!');
@@ -13,7 +20,7 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.author.bot) return;
 
-  const m = message.content.toLowerCase().trim();
+  const m = message.content.trim();
 
   if(m.indexOf(':') !== -1) {
     let guildID = message.guild.id;
@@ -27,89 +34,13 @@ client.on('message', message => {
     if (message.member.permissions.has("ADMINISTRATOR")) {
       //console.log("Member is owner: " + (message.member === message.guild.owner))
       if(command === "bee_movie_script" && message.member === message.guild.owner){
-        message.channel.send("Give me a sec...");
-        let script = beeMovie.script.split('');
-        //console.log(script.length)
-        let scriptPieces = [];
-        let lettercount = 0;
-        let mes = '';
-        for(let i = 0; i < script.length; i++) {
-          if (lettercount % 10000 === 0 && lettercount !== 0) {
-            message.channel.send("Letter Count: " + lettercount)
-          }
-          if (lettercount < 2000 && i < script.length - 1) {
-            mes += script[i]
-            //console.log(script[i])
-            lettercount += 1;
-          } else {
-            if (i === script.length - 1) {
-              mes += script[i]
-            }
-            scriptPieces.push(mes);
-            mes = script[i];
-            lettercount = 1;
-          }
-        }
-        for(i = 0; i < scriptPieces.length; i++) {
-          if (scriptPieces[i] !== '') {
-            message.channel.send(scriptPieces[i]);
-          }
-        }
-        return;
+        beeMovieScript(message)
       }
       if(command === "unban") {
-        if(args.length > 0) {
-          let unbanned_words = [];
-          for(let i = 0; i < args.length; i++) {
-            unbanned_words.push(args[i].trim())
-          }
-          let real_unbaned_words = [];
-          for(let i = 0; i < non_christian_words.length; i++) {
-            if(unbanned_words.indexOf(non_christian_words[i]) !== -1) {
-              real_unbaned_words.push(non_christian_words[i]);
-              non_christian_words.splice(i, 1);
-              i -= 1;
-            }
-          }
-          if(real_unbaned_words.length > 0) {
-            let words_to_write = non_christian_words.join('|');
-            fs.writeFile(`./${guildID}.txt`, words_to_write, function(err) {
-              if(err) {
-                message.channel.send('There was an error unbanning the words.')
-              }
-              retrieveNonChristianWords(guildID);
-              message.channel.send('Okay the following words have been unbanned: **' + real_unbaned_words.join(', ') + '**')
-            }); 
-          } else {
-            message.channel.send('Hmm... looks like none of those words are currently banned.')      
-          }
-        } else {
-          message.channel.send("Hmmm... It seems you didn't give me a word to unban")
-        }
+        unbanWords(message, args)
       }
       if(command === "ban") {
-        if(args.length > 0) {
-          let banned_words = [];
-          for(let i = 0; i < args.length; i++) {
-            banned_words.push(args[i].trim())
-          }
-          for(let i = 0; i < banned_words.length; i++) {
-            if(non_christian_words.indexOf(banned_words[i]) !== -1) {
-              banned_words.splice(i, 1);
-              i -= 1;
-            }
-          }
-          let words_to_write = non_christian_words.concat(banned_words).join('|');
-          fs.writeFile(`./${guildID}.txt`, words_to_write, function(err) {
-            if(err) {
-              message.channel.send('There was an error saving the banned words.')
-            }
-            retrieveNonChristianWords(guildID);
-            message.channel.send('Okay the following words have been banned: **' + banned_words.join(', ') + '**')
-          }); 
-        } else {
-          message.channel.send("Hmmm... It seems you didn't give me a word to ban")
-        }
+        banWords(message, args)
       }
       if(command === "list all") {
         if(non_christian_words.length > 0) {
@@ -128,9 +59,23 @@ client.on('message', message => {
       if(command === "help") {
         message.channel.send('**Barry B. Benson Bot Help**\n`ban: words` - bans words after the `:`, use commas to separate words/phrases\n`unban: words` - unbans words  after the `:` if they are currently in ban list, use commas to separate words/phrases\n`list all:` - lists all currently banned words\n`set timeout: number` - sets bot message tiemout in seconds\n`help:` - dsiplays this help screen')
       }
+      if(command === "join") {
+        message.reply("Attempting to join channel: " + args[0]);
+        JoinChannel(args[0], message);
+      }
+      if(command === "play") {
+        PlayCommand(args.join(" "), message);
+      }
     }
   }
+  checkForBannedWords(message)
+});
 
+
+
+client.login(process.env.TOKEN);
+
+function checkForBannedWords(message) {
   let banned_words_used = [];
   for(let word of non_christian_words) {
     if(message.content.toLowerCase().includes(word)) {
@@ -156,9 +101,118 @@ client.on('message', message => {
       })
     })
   }
-});
+}
 
-client.login(process.env.TOKEN);
+function beeMovieScript(message) {
+  message.channel.send("Give me a sec...");
+  let script = beeMovie.script.split('');
+  //console.log(script.length)
+  let scriptPieces = [];
+  let lettercount = 0;
+  let mes = '';
+  for(let i = 0; i < script.length; i++) {
+    if (lettercount % 10000 === 0 && lettercount !== 0) {
+      message.channel.send("Letter Count: " + lettercount)
+    }
+    if (lettercount < 2000 && i < script.length - 1) {
+      mes += script[i]
+      //console.log(script[i])
+      lettercount += 1;
+    } else {
+      if (i === script.length - 1) {
+        mes += script[i]
+      }
+      scriptPieces.push(mes);
+      mes = script[i];
+      lettercount = 1;
+    }
+  }
+  for(i = 0; i < scriptPieces.length; i++) {
+    if (scriptPieces[i] !== '') {
+      message.channel.send(scriptPieces[i]);
+    }
+  }
+  return;
+}
+
+function unbanWords(message, args) {
+  if(args.length > 0) {
+    let unbanned_words = [];
+    for(let i = 0; i < args.length; i++) {
+      unbanned_words.push(args[i].trim())
+    }
+    let real_unbaned_words = [];
+    for(let i = 0; i < non_christian_words.length; i++) {
+      if(unbanned_words.indexOf(non_christian_words[i]) !== -1) {
+        real_unbaned_words.push(non_christian_words[i]);
+        non_christian_words.splice(i, 1);
+        i -= 1;
+      }
+    }
+    if(real_unbaned_words.length > 0) {
+      let words_to_write = non_christian_words.join('|');
+      fs.writeFile(`./${guildID}.txt`, words_to_write, function(err) {
+        if(err) {
+          message.channel.send('There was an error unbanning the words.')
+        }
+        retrieveNonChristianWords(guildID);
+        message.channel.send('Okay the following words have been unbanned: **' + real_unbaned_words.join(', ') + '**')
+      }); 
+    } else {
+      message.channel.send('Hmm... looks like none of those words are currently banned.')      
+    }
+  } else {
+    message.channel.send("Hmmm... It seems you didn't give me a word to unban")
+  }
+}
+
+function banWords(message, args) {
+  if(args.length > 0) {
+    let banned_words = [];
+    for(let i = 0; i < args.length; i++) {
+      banned_words.push(args[i].trim())
+    }
+    for(let i = 0; i < banned_words.length; i++) {
+      if(non_christian_words.indexOf(banned_words[i]) !== -1) {
+        banned_words.splice(i, 1);
+        i -= 1;
+      }
+    }
+    let words_to_write = non_christian_words.concat(banned_words).join('|');
+    fs.writeFile(`./${guildID}.txt`, words_to_write, function(err) {
+      if(err) {
+        message.channel.send('There was an error saving the banned words.')
+      }
+      retrieveNonChristianWords(guildID);
+      message.channel.send('Okay the following words have been banned: **' + banned_words.join(', ') + '**')
+    }); 
+  } else {
+    message.channel.send("Hmmm... It seems you didn't give me a word to ban")
+  }
+}
+
+function JoinChannel(channel, message) {
+  if(voiceChannel !== null) {
+    console.log('DISCONNECTED FROM VOICE')
+    voiceChannel.leave();
+  }
+
+  voiceChannel = GetChannelByName(channel.trim());
+
+  if(voiceChannel) {
+    console.log('Joining voice channel')
+    return voiceChannel.join().then(connection => {
+      message.channel.send("Connected to voice channel: " + channel.trim())
+    }).catch(err => {
+      console.error(err)
+    });;
+  } else {
+    message.reply("Couldn't find channel: " + channel.trim());
+    return;
+  }
+}
+
+// Helper functions
 
 function retrieveNonChristianWords(guildID) {
   if (!fs.existsSync(`./${guildID}.txt`)) {
@@ -166,4 +220,75 @@ function retrieveNonChristianWords(guildID) {
   }
   non_christian_words = fs.readFileSync(`./${guildID}.txt`, { encoding: 'utf8' }).split('|').filter(String);
   //console.log(non_christian_words)
+}
+
+function GetChannelByName(name) {
+  console.log('Finding Voice Channel: ' + name)
+  let channel = client.channels.find(val => val.name === name);
+  if(channel) {
+    console.log('Channel "' + name + '" found')
+  }
+  return channel;
+}
+
+function PlayCommand(searchTerm, message) {
+  message.channel.send("Searching Youtube for audio...");
+  YoutubeSearch(searchTerm, message);
+}
+
+function YoutubeSearch(searchKeywords, message) {
+  var requestUrl = 'https://www.googleapis.com/youtube/v3/search' + `?part=snippet&q=${escape(searchKeywords)}&key=${process.env.API_KEY}`;
+
+  request(requestUrl, (error, response) => {
+      if (!error && response.statusCode == 200) {
+
+          var body = response.body;
+          if (body.items.length == 0) {
+              message.channel.send("Your search gave 0 results");
+              return videoId;
+          }
+
+          for (var item of body.items) {
+              if (item.id.kind === 'youtube#video') {
+                console.log("Queued: " + item.id.videoId)
+                  QueueYtAudioStream(item.id.videoId);
+              }
+          }
+      }
+      else {
+          console.log("Unexpected error when searching YouTube");
+          return null;
+      }
+  });
+
+  return null;
+}
+
+/// Queues result of Youtube search into stream
+function QueueYtAudioStream(videoId) {
+  var streamUrl = `https://www.youtube.com/watch?v=${videoId}`;
+  ytAudioQueue.push(streamUrl);
+  if(!playing) {
+    PlayStream(ytAudioQueue[0]);
+  }
+}
+
+function PlayStream(streamUrl) {
+  playing = true;
+  const streamOptions = {seek: 0, volume: 1};
+  console.log("Streaming audio from " + streamUrl);
+
+  if (streamUrl && !playing) {
+      const stream = ytdl(streamUrl, {filter: 'audioonly'});
+      const dispatcher = client.voiceConnections.first().playStream(stream, streamOptions);
+      dispatcher.on('end', () => {
+        playing = false;
+        if(ytAudioQueue > 0) {
+          ytAudioQueue.shift();
+          PlayStream(ytAudioQueue[0]);
+        } else {
+          PlayStream('https://www.youtube.com/watch?v=AY3OX809dzM');
+        }
+      })
+  }
 }
