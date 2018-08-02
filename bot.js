ab9d9937ee60b01bcaaba8dee612f1d617ab5ab1
BradHacker <harkerbd@gmail.com>
2018-08-02 19:17:54 -0400

    Debug Logging

--- bot.js
+++ bot.js
@@ -304,10 +304,14 @@ function PlayStream(video) {
     if(channel) {
       channel.send('Now Playing: ' + video.title).then(() => {
         console.log("Sent message to announcements")
-        console.log("Streaming audio from " + video.url);
+      }).catch(() => {
+        console.error("Couldn't send announcement")
+      })
+      console.log("Streaming audio from " + video.url);
         const stream = ytdl(video.url, {filter: 'audioonly'});
         const dispatcher = client.voiceConnections.first().playStream(stream, streamOptions);
         dispatcher.on('end', () => {
+          console.log('Song Ended')
           playing = false;
           if(ytAudioQueue > 0) {
             ytAudioQueue.shift();
@@ -315,9 +319,9 @@ function PlayStream(video) {
           }
           //ListQueue();
         })
-      }).catch(() => {
-        console.error("Couldn't send announcement")
-      })
+        dispatcher.on('speaking', () => {
+          console.log('Speaking')
+        })
     } else {
       console.error("Couldn't connect to announcement channel")
       let channel = client.channels.find(val => val.name.toLower() === "general")
